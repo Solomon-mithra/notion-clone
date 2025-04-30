d381ea9056f3ca1f7f9afa7cfd8e113ffd2fea9e
Solomon Mithra <solomonmithra99@gmail.com>
2025-04-29 22:57:49 -0700

    auth added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 
 export const metadata: Metadata = {
@@ -17,6 +24,15 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
         {children}
       </body>
     </html>
