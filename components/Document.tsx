a10f435620d8470e4cb257208a43d56ab3185c89
Solomon Mithra <solomonmithra99@gmail.com>
2025-06-14 02:43:27 -0700

    Text editor added

--- components/Document.tsx
+++ components/Document.tsx
@@ -32,7 +32,7 @@ function Document({ id }: { id: string }) {
     };
 
     return (
-        <div>
+        <div className="flex-1 h-full bg-white p-5">
             <div className="flex max-w-6xl mx-auto justify-between pb-5">
                 <form className="flex flex-1 space-x-2" onSubmit={updateTitle}>
                     {/* update title */}
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,29 +1,94 @@
+import { useRoom, useSelf } from '@liveblocks/react'
+import { useEffect, useState } from 'react'
+import * as Y from 'yjs'
+import { LiveblocksYjsProvider } from '@liveblocks/yjs'
+import { MoonIcon, SunIcon } from 'lucide-react'
+import { Button } from './ui/button'
+import { BlockNoteView } from '@blocknote/shadcn'
+import { BlockNoteEditor } from '@blocknote/core'
+import { useCreateBlockNote } from '@blocknote/react'
+import '@blocknote/shadcn/style.css'
+import '@blocknote/core/fonts/inter.css'
+import stringToColor from '@/lib/stringToColor'
 
-import { useRoom } from "@liveblocks/react";
-import { useState } from "react";
-import * as Y from "yjs";
+type EditorProps = {
+    doc: Y.Doc
+    provider: any
+    darkMode: boolean
+}
+
+function BlockNote({ doc, provider, darkMode }: EditorProps) {
+    const userInfo = useSelf((me) => me.info)
+    const editor: BlockNoteEditor = useCreateBlockNote({
+        collaboration: {
+            provider,
+            fragment: doc.getXmlFragment('document-store'),
+            user: {
+                name: userInfo?.name ?? 'Anonymous',
+                color: stringToColor(userInfo?.email || ''),
+            },
+        },
+    })
+
+    return (
+        <div className="relative max-w-6xl mx-auto">
+            <BlockNoteView
+                className="nim-h-screen"
+                editor={editor}
+                theme={darkMode ? 'dark' : 'light'}
+            />
+        </div>
+    )
+}
 
 function Editor() {
-
     const room = useRoom();
     const [doc, setDoc] = useState<Y.Doc>();
+    const [provider, setProvider] = useState<LiveblocksYjsProvider>();
+    const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+        const ydoc = new Y.Doc();
+        const provider = new LiveblocksYjsProvider(room, ydoc);
+        setDoc(ydoc);
+        setProvider(provider);
 
-  return (
-    <div className="max-w-6xl mx-auto">
-        <div>
-            {/* TranslateDocument AI */}
+        return () => {
+            provider?.destroy();
+            ydoc?.destroy();
+        };
+    }, [room]);
 
-            {/* ChatToDocument AI */}
+    const style = `hover:text-white ${
+        darkMode
+            ? 'text-gray-300 bg-gray-700 hover:bg-gray-100 hover:text-gray-700'
+            : 'text-gray-700 bg-gray-200 hover:bg-gray-300 hover:text-gray-700'
+    }`;
 
-            {/* {Dark Mode} */}
+    return (
+        <div className="max-w-6xl mx-auto">
+            <div className="flex items-center gap-2 justify-end mb-6">
+                {/* TranslateDocument AI */}
+
+                {/* ChatToDocument AI */}
+
+                {/* {Dark Mode} */}
+                <Button
+                    className={style}
+                    onClick={() => setDarkMode(!darkMode)}
+                >
+                    {darkMode ? (
+                        <SunIcon className="h-4 w-4" />
+                    ) : (
+                        <MoonIcon className="h-4 w-4" />
+                    )}
+                </Button>
+            </div>
+            {/* BlockNote */}
+            {doc && provider && (
+                <BlockNote doc={doc} provider={provider} darkMode={darkMode} />
+            )}
         </div>
-        {/* BlockNote */}
-    </div>
     )
 }
 
 export default Editor
-
-function usesState<T>(): [any, any] {
-    throw new Error("Function not implemented.");
-}
